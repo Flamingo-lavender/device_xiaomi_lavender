a4f348777e0f30b1177d163e09d077edd3038ae8
xenxynon <flynryder427@gmail.com>
2022-06-16 13:10:41 +0530

    lavender: arrow --> flamingo
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_lavender.mk
+    $(LOCAL_DIR)/flamingo_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_lavender-user \
-    arrow_lavender-userdebug \
-    arrow_lavender-eng
+    flamingo_lavender-userdebug
--- flamingo_lavender.mk
+++ flamingo_lavender.mk
@@ -4,25 +4,31 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Check for target product
+ifeq (flamingo_lavender,$(TARGET_PRODUCT))
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit FlamingoOS configuration
+$(call inherit-product, vendor/flamingo/target/product/flamingo.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Common Device Propeties
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := arrow_lavender
-PRODUCT_MODEL := Redmi Note 7
-
-DEVICE_MAINTAINER := Ratoriku, Hazama25
+PRODUCT_NAME := flamingo_lavender
+TARGET_VENDOR_PRODUCT_NAME := lavender
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-TARGET_VENDOR_PRODUCT_NAME := lavender
+TARGET_BUILD_LAWNCHAIR := false
+TARGET_BUILD_MATLOG := false
+OFFICIAL_BUILD := true
+
+endif
